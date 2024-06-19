efe4ef0c160710db73f07ff7d4b39c915d31c7fe
SamuelJJk <samueljean.j24@gmail.com>
2024-06-19 16:31:49 -0400

    completed the brief summary for the main page

--- Client/src/Components/Summary.jsx
+++ Client/src/Components/Summary.jsx
@@ -6,7 +6,9 @@ function Summary() {
       <div className='briefIntro'>
         Hello I'm Daniel, a passionate photographer with a keen eye for capturing the beauty in everyday moments. Specializing in weddings,busniess photos and modeling. I strive to tell compelling stories through my lens. My work blends creativity, technical skill, and an authentic connection with my subjects, resulting in images that are not only visually stunning but also deeply meaningful. Explore my portfolio to see the world through my eyes and experience the art of photography in its purest form.
       </div>
-      <img src="" alt="" className="bustShot" />
+      <div className="profileContainer">
+        <img src="https://images.unsplash.com/photo-1712738603891-3f504a0d2658?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="profileImg" />
+      </div>
     </div>
   )
 }
