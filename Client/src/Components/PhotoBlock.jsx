e16330069838f5424994de268c8664e51c8a06b9
SamuelJJk <samueljean.j24@gmail.com>
2024-06-21 20:41:00 -0400

    created state that hold an image

--- Client/src/Components/PhotoBlock.jsx
+++ Client/src/Components/PhotoBlock.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function PhotoBlock() {
+    const [displayPhoto,setDisplayPhoto]=useState({});
+
+    useEffect(()=>{
+        setDisplayPhoto({
+            image:"https://plus.unsplash.com/premium_photo-1687653085814-4c138d968688?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        });
+    },[]);
   return (
     <div className='photoBlock'>
-      <img src="" alt="" className="photoblockImg" />
+      <img src={displayPhoto.image} alt="" className="photoBlockImg" />
     </div>
   )
 }
