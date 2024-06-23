a77b17dacdbf3b3b4ca2c5680a670de33d87b5ba
SamuelJJk <samueljean.j24@gmail.com>
2024-06-23 09:07:20 -0400

    updated code

--- Client/src/Pages/Main.jsx
+++ Client/src/Pages/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Style/Main.css'
 import Hero from '../Components/Hero'
 import Summary from '../Components/Summary'
@@ -7,9 +7,23 @@ import FeaturedWork from '../Components/FeaturedWork'
 import PhotoDisplay from '../Components/PhotoDisplay'
 
 function Main() {
+  const [displayModal,setDisplayModal]=useState({
+    toggle:false,
+    style:{
+      display:"flex"
+    }
+  });
+  const toggleModal =()=>{
+    setDisplayModal({toggle:prevState =>!prevState})
+    if(displayModal.toggle){
+      setDisplayModal({style:{display:"flex"}})
+    }else{
+      setDisplayModal({style:{display:"none"}})
+    }
+  }
   return (
     <div className='index'>
-      <PhotoDisplay/>
+      <PhotoDisplay toggleModal={toggleModal} displayModal={displayModal}/>
       <Hero/>
       <Summary/>
       <FeaturedWork/>
